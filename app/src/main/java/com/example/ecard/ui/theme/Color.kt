fb4fe7a6564606e6bb7f30143b622ef290e22712
Thao Le Van <85724290+thaolv-03@users.noreply.github.com>
2023-04-16 15:38:01 +0700

    upload

---
+++ app/src/main/java/com/example/ecard/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.ecard.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Light Theme
+val md_theme_light_background = Color(0xFFFFFFFF)
+val md_theme_light_surface = Color(0xFFF4F4F4)
+val md_theme_light_onSurface1 = Color(0xFF222222)
+val md_theme_light_onSurface2 = Color(0xFF8A8A8A)
+val md_theme_light_primary = Color(0xFFFFFFFF)
+val md_theme_light_onPrimary1 = Color(0xFF222222)
+val md_theme_light_onPrimary2 = Color(0xFF005CC9)
+val md_theme_light_secondary = Color(0xFF005CC9)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+
+// Dark Theme
+val md_theme_dark_background = Color(0xFF1B1C18)
+val md_theme_dark_surface = Color(0xFF323232)
+val md_theme_dark_onSurface1 = Color(0xFFFFFFFF)
+val md_theme_dark_onSurface2 = Color(0xFFF2F2F2)
+val md_theme_dark_primary = Color(0xFF1B1C18)
+val md_theme_dark_onPrimary1 = Color(0xFFFFFFFF)
+val md_theme_dark_onPrimary2 = Color(0xFF2184F9)
+val md_theme_dark_secondary = Color(0xFF005CC9)
+val md_theme_dark_onSecondary = Color(0xFFFFFFFF)
