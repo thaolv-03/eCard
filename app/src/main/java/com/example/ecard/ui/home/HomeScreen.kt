c6900ad4b757501649c9c0d1661183ba9a6e08c9
DEL <lucvt.21it@vku.udn.vn>
2023-04-16 16:14:40 +0700

    upload

---
+++ app/src/main/java/com/example/ecard/ui/home/HomeScreen.kt
@@ -0,0 +1,44 @@
+package com.example.ecard.ui.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ecard.ui.theme.ECardTheme
+
+@Composable
+fun ImageAndName(image: Painter, description: String, name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = description,
+            modifier = Modifier
+                .clip(CircleShape)
+        )
+
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h3,
+            textAlign = TextAlign.Center
+        )
+    }
+    
+}
+
+@Preview
+@Composable
+fun Preview1() {
+    ECardTheme() {
+        Surface() {
+//            ImageAndName(image = , description = , name = )
+        }
+    }
+}
